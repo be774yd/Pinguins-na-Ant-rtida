8482d499344f720b354fc9904bff6844a8f58a07
izabella gomes <izabellagomes121@icloud.com>
2025-07-19 03:19:40 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var menuIcon = document.querySelector('.menu-icon');
+var ul = document.querySelector('.ul');
+
+menuIcon.addEventListener('click',()=>{
+
+    if (ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo');
+        
+        
+    }else{
+        ul.classList.add('ativo');
+       
+    }
+    
+
+
+    })
